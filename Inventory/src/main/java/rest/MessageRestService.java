56284d81533ab4dd93401a95ef7941adf712f97a
jnikam66 <jnikam66@gmail.com>
2017-09-22 11:34:36 -0700

    Added database.

--- Inventory/src/main/java/rest/MessageRestService.java
+++ Inventory/src/main/java/rest/MessageRestService.java
@@ -1,5 +1,11 @@
 package rest;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -10,9 +16,18 @@ public class MessageRestService {
 
 	@GET
 	@Path("/{param}")
-	public Response printMessage(@PathParam("param") String msg) {
+	public Response printMessage(@PathParam("param") String msg) throws ClassNotFoundException, SQLException {
+		Class.forName("org.h2.Driver");
+		Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+		Statement stmt=conn.createStatement();  
+		ResultSet rs=stmt.executeQuery("select username from login");  
+		String result = null;
+		if (rs.next()) {
+			result = "Restful example : " + msg + rs.getString(1);
+			System.out.println( msg + rs.getString(1));
 
-		String result = "Restful example : " + msg;
+	    }
+		conn.close();
 
 		return Response.status(200).entity(result).build();
 
