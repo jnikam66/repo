030a19a17db46134895d0b7daa05e043df1ca0b3
jnikam66 <jnikam66@gmail.com>
2017-09-26 23:55:20 -0700

    Persistence

--- Inventory/src/main/java/rest/MessageRestService.java
+++ Inventory/src/main/java/rest/MessageRestService.java
@@ -1,11 +1,11 @@
 package rest;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.Query;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -13,24 +13,18 @@ import javax.ws.rs.core.Response;
 
 @Path("/message")
 public class MessageRestService {
+	@PersistenceUnit(name = "jpa-inventory") 
+	private EntityManagerFactory entityManagerFactory;
 
+	public EntityManager getEntityManager(){
+		return  entityManagerFactory.createEntityManager();
+	}
 	@GET
 	@Path("/{param}")
 	public Response printMessage(@PathParam("param") String msg) throws ClassNotFoundException, SQLException {
-		Class.forName("org.h2.Driver");
-		Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
-		Statement stmt=conn.createStatement();  
-		ResultSet rs=stmt.executeQuery("select username from login");  
-		String result = null;
-		if (rs.next()) {
-			result = "Restful example : " + msg + rs.getString(1);
-			System.out.println( msg + rs.getString(1));
 
-	    }
-		conn.close();
-
-		return Response.status(200).entity(result).build();
-
+		Query query = this.getEntityManager().createQuery("select username from login");
+        return (Response) query.getResultList(); 
 	}
 
 }
