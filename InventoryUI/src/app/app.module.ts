b105208ac75d3d355072ea159155ba6cd7717211
jnikam66 <jnikam66@gmail.com>
2017-10-03 14:17:12 -0700

    Added dashboard module in Inventory UI.

--- InventoryUI/src/app/app.module.ts
+++ InventoryUI/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { DashboardModule } from './dashboard/dashboard.module'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
