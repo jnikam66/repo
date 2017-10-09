dd06a07bd92de0f3eef2ef72106af833171c83c5
jnikam66 <jnikam66@gmail.com>
2017-10-09 13:54:55 -0700

    Register service.

--- Inventory/src/main/java/rest/AuthenticateService.java
+++ Inventory/src/main/java/rest/AuthenticateService.java
@@ -1,25 +1,25 @@
 package rest;
 
 import java.sql.SQLException;
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+import javax.transaction.SystemException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.transaction.Transactional; 
 
-@Path("/authenticate")
+@Path("/userServices")
 public class AuthenticateService {
 	EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-inventory");
+	EntityManager entityManager = emf.createEntityManager();
 
 	@GET
-	@Path("/{username}/{password}")
+	@Path("/authenticate/{username}/{password}")
 	public Boolean authenticateUser(@PathParam("username") String username, @PathParam("password") String password)
 			throws ClassNotFoundException, SQLException {
-		EntityManager entityManager = emf.createEntityManager();
 		Query query = entityManager.createNativeQuery("select l.password from login l where l.username = :username");
 		query.setParameter("username", username.toUpperCase());
 		if (query.getResultList().get(0).toString().equals(password.toUpperCase())) {
@@ -28,5 +28,29 @@ public class AuthenticateService {
 			return false;
 		}
 
+	}
+	@Transactional() 
+	@GET
+	@Path("/register/{username}/{password}")
+	public String register(@PathParam("username") String email, @PathParam("password") String password)
+			throws Exception, SecurityException, SystemException {
+		String result = "false";
+		try {
+			entityManager.getTransaction().begin(); 
+
+			Query query = entityManager.createNativeQuery("insert into login(id,username,password) values(?,?,?)");
+			query.setParameter(1, 10);
+			query.setParameter(2, email);
+			query.setParameter(3, password);
+			if (query.executeUpdate() > 0) {
+				result = "Transaction Complete";
+			}
+			entityManager.getTransaction().commit();
+		    entityManager.close(); 
+		} catch (Exception e) {
+			e.printStackTrace();
+			result = e.getMessage();
+		}
+		return result;
 	}
 }
