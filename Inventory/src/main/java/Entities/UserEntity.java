19992943f74823e8df27d8e65fd07a9e82b842e2
snehalsj13 <snehal.snehal4@gmail.com>
2017-10-11 00:22:00 -0700

    Added user entity.

---
+++ Inventory/src/main/java/Entities/UserEntity.java
@@ -0,0 +1,125 @@
+package Entities;
+
+import java.io.Serializable;
+import java.sql.Blob;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="User")
+@SuppressWarnings("unused")
+public class UserEntity implements Serializable{
+	@Id
+	@Column(name="USERNAME")
+	private String username;
+	
+	@Column(name="PASSWORD")
+	private String password;
+	
+	@Column(name="ACCESSLEVEL")
+	private String accessLevel;
+	
+	@Column(name="COMPANY")
+	private String company;
+	
+	@Column(name="COUNTRY")
+	private String country;
+	
+	@Column(name="EMAIL")
+	private String email;
+	
+	@Column(name="CONTACTNUMBER")
+	private String contactNumber;
+	
+	@Column(name="HASHKEY")
+	private String hashKey;
+	
+	@Column(name="ISACTIVE")
+	private Boolean isActive;
+	
+	@Column(name="PROFILEPICTURE")
+	private Blob profilePicture;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAccessLevel() {
+		return accessLevel;
+	}
+
+	public void setAccessLevel(String accessLevel) {
+		this.accessLevel = accessLevel;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	public String getHashKey() {
+		return hashKey;
+	}
+
+	public void setHashKey(String hashKey) {
+		this.hashKey = hashKey;
+	}
+
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public Blob getProfilePicture() {
+		return profilePicture;
+	}
+
+	public void setProfilePicture(Blob profilePicture) {
+		this.profilePicture = profilePicture;
+	}
+}
--- Inventory/src/main/java/rest/AuthenticateService.java
+++ Inventory/src/main/java/rest/AuthenticateService.java
@@ -1,15 +1,19 @@
 package rest;
 
 import java.sql.SQLException;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 import javax.transaction.SystemException;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.transaction.Transactional; 
+
+import Entities.UserEntity;
 
 @Path("/userServices")
 public class AuthenticateService {
@@ -30,15 +34,30 @@ public class AuthenticateService {
 
 	}
 	@Transactional() 
-	@GET
-	@Path("/register/{username}/{password}")
-	public String register(@PathParam("username") String email, @PathParam("password") String password)
-			throws Exception, SecurityException, SystemException {
-		String result = "false";
+	@POST	
+	@Path("/register/")
+	//@Path("/register/{username}/{password}")
+	//public String register(@PathParam("username") String email, @PathParam("password") String password)
+			//throws Exception, SecurityException, SystemException {
+	public String register(UserEntity userEntityRecord) throws Exception, SecurityException, SystemException {
+		String result = "Registering User...";
 		try {
 			entityManager.getTransaction().begin(); 
-
-			Query query = entityManager.createNativeQuery("insert into login(id,username,password) values(?,?,?)");
+			
+			/*UserEntity userRec = new UserEntity();
+			userRec = entityManager.find(User.class,userEntityRecord.getUsername());
+			if(userRec!=null){
+				result = "Duplicate user!!";
+			}else{
+				*/
+				entityManager.persist(userEntityRecord); 
+				entityManager.getTransaction().commit();
+				result = "Transaction Complete";
+			//}
+			entityManager.close();
+			
+			/*
+			Query query = entityManager.createNativeQuery(	"insert into login(id,username,password) values(?,?,?)");
 			query.setParameter(1, 10);
 			query.setParameter(2, email);
 			query.setParameter(3, password);
@@ -47,6 +66,8 @@ public class AuthenticateService {
 			}
 			entityManager.getTransaction().commit();
 		    entityManager.close(); 
+			*/
+		     
 		} catch (Exception e) {
 			e.printStackTrace();
 			result = e.getMessage();
