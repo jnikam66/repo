15c3ee6658ce055477866a3ffdf258c840413aa8
jnikam66 <jnikam66@gmail.com>
2017-10-11 18:23:47 -0700

    Features

--- InventoryUI/src/app/app.routing.ts
+++ InventoryUI/src/app/app.routing.ts
@@ -5,11 +5,15 @@ import { DashboardComponent } from './dashboard/dashboard.component'
 import { LoginComponent }  from './login/login.component';
 import { RegisterComponent } from './register/register.component'
 import { InventoryComponent } from './features/inventory/inventory.component'
+import { FeaturesComponent } from './features/features.component'
+import { ReportsComponent } from './features/reports/reports.component'
 export const AppRoutes: any = [
     {path: '' , component : DashboardComponent },
     { path: 'login', component: LoginComponent },
     { path : 'register', component : RegisterComponent },
-    { path : 'inventory', component : InventoryComponent }
+    { path : 'inventory', component : InventoryComponent },
+    { path : 'features', component : FeaturesComponent},
+    { path : 'reports', component :ReportsComponent}
 ];
 
 export const AppComponents: any = [
--- InventoryUI/src/app/features/features.module.ts
+++ InventoryUI/src/app/features/features.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from "@angular/router";
-import { AppComponents, AppRoutes} from "../app.routing";
-import { HeaderComponent } from '../header/header.component'
+import { FeaturesComponent } from './features.component'
+//import { AppComponents, AppRoutes} from "../app.routing";
+import { HeaderComponent } from './header/header.component'
 import { InventoryComponent } from './inventory/inventory.component';
+import { ReportsComponent } from './reports/reports.component';
+
 
 @NgModule({
 imports: [
   CommonModule,
-  RouterModule,
-  RouterModule.forRoot(AppRoutes)
+  RouterModule
+  //RouterModule.forRoot(AppRoutes)
 ],
 declarations: [
-  AppComponents,
+//  AppComponents,
+  FeaturesComponent,
   HeaderComponent,
-  InventoryComponent
+  InventoryComponent,
+  ReportsComponent
 ]
 })
 export class FeaturesModule { }
