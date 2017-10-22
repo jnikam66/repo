f6f44e54ebee5a1e659dac7004add6e1544cd18a
jnikam66 <jnikam66@gmail.com>
2017-10-22 00:03:24 -0700

    Inventory module.

--- InventoryUI/src/app/features/inventory/inventory.component.ts
+++ InventoryUI/src/app/features/inventory/inventory.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {DataSource} from '@angular/cdk/collections';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
   styleUrls: ['./inventory.component.css']
 })
+
 export class InventoryComponent implements OnInit {
 
+
+   data ={
+    name :'' ,
+    email:'',
+    age:'',
+    city:''
+  }
   constructor() { }
 
   ngOnInit() {
